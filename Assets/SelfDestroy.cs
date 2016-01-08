28716839af1fc3d8e6b09c1a8ed3463a36a9bdad
Manuel Herrmann <mherrmann1508@web.de>
2016-01-08 01:50:48 +0100

    oink

--- Assets/SelfDestroy.cs
+++ Assets/SelfDestroy.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class SelfDestroy : MonoBehaviour
 {
-    float timer = 2.8f;
+    float timer = 3f;
+    ParticleSystem psy;
     // Use this for initialization
     void Start()
     {
-        this.GetComponent<ParticleSystem>().Play();
+        psy = this.GetComponent<ParticleSystem>();
+        psy.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
         this.timer -= Time.deltaTime;
-        if (timer <= 0)
+        if (psy.particleCount < 10 && this.timer <= 0)
             Destroy(this.gameObject);
     }
 }
