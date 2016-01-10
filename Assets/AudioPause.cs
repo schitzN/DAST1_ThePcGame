5726f608020ddbcd6d83c73973a15a4e95ddb79a
Manuel Herrmann <mherrmann1508@web.de>
2016-01-10 22:40:09 +0100

    adsd

---
+++ Assets/AudioPause.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPause : MonoBehaviour {
+    AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        this.audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.paused && audio.isPlaying)
+            audio.Pause();
+        else if (!GameManager.paused && !audio.isPlaying)
+            audio.Play();
+	}
+}
